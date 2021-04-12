f03b816e9a5190741e1527900d3b1e1018e96a55
savannahvaith <sav.vaithilingam@gmail.com>
2021-04-12 15:00:18 +0100

    SV: Initial Commit

---
+++ src/main/java/com/qa/springpups/SpringPupsApplication.java
@@ -0,0 +1,13 @@
+package com.qa.springpups;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPupsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPupsApplication.class, args);
+	}
+
+}
