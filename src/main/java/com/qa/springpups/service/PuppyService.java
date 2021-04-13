9c5a26e9180c21463547dc49eed76663a834173a
savannahvaith <sav.vaithilingam@gmail.com>
2021-04-13 09:41:59 +0100

    SV: Updated service file - removed interface

--- src/main/java/com/qa/springpups/service/PuppyService.java
+++ src/main/java/com/qa/springpups/service/PuppyService.java
@@ -1,21 +1,64 @@
 package com.qa.springpups.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
 
 import com.qa.springpups.domain.Puppy;
 import com.qa.springpups.dto.PuppyDTO;
+import com.qa.springpups.repo.PuppyRepo;
 
-public interface PuppyService {
+@Service
+public class PuppyService{
 	
-	PuppyDTO createPuppy(Puppy p);
+	private PuppyRepo repo; 
+	private ModelMapper mapper; 
 	
-	List<PuppyDTO> readPuppies();
+	public PuppyService(PuppyRepo repo, ModelMapper mapper) {
+		this.repo = repo; 
+		this.mapper = mapper;
+	}
 	
-	PuppyDTO getPuppyById(Long id);
+	public PuppyDTO mapToDTO(Puppy pup) {
+		return this.mapper.map(pup, PuppyDTO.class);
+	}
 	
-	PuppyDTO updatePuppy(Long id, PuppyDTO newPuppy);
 	
-	boolean deletePuppy(Long id);
-	
-	PuppyDTO mapToDTO(Puppy pup);
+	public PuppyDTO create(Puppy p) {
+		return this.mapToDTO(this.repo.save(p));
+	}
+
+	public List<PuppyDTO> read() {
+		List<PuppyDTO> dtos = new ArrayList<>(); 
+		for(Puppy pup : this.repo.findAll()) {
+			dtos.add(this.mapToDTO(pup));
+		}
+		return dtos; 
+//		return  this.repo.findAll().stream().map(this::mapToDTO).collect(Collectors.toList());
+	}
+
+	public PuppyDTO getById(Long id) {
+		Optional<Puppy> optPup = this.repo.findById(id);
+		return this.mapToDTO(optPup.orElse(null));
+	}
+
+	public PuppyDTO update(Long id, PuppyDTO newPuppy) {
+		Puppy existing = this.repo.findById(id).orElseThrow();
+		
+		existing.setAge(newPuppy.getAge());
+		existing.setBreed(newPuppy.getBreed());
+		existing.setName(newPuppy.getName());
+		existing.setOwner(newPuppy.getOwner());
+		
+		return this.mapToDTO(this.repo.save(existing));
+	}
+
+	public boolean delete(Long id) {
+		this.repo.deleteById(id);
+		return !this.repo.existsById(id);
+	}
 }
