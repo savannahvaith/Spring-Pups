76adaef7d1420e65273a4c5ede9677c07accede2
savannahvaith <sav.vaithilingam@gmail.com>
2021-04-15 15:34:43 +0100

    SV: Finished integration testing for Owner

--- src/test/java/com/qa/springpups/controller/OwnerControllerIntegrationTest.java
+++ src/test/java/com/qa/springpups/controller/OwnerControllerIntegrationTest.java
@@ -1,6 +1,5 @@
 package com.qa.springpups.controller;
 
-import static org.assertj.core.api.Assertions.setAllowComparingPrivateFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -19,6 +18,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -29,6 +30,7 @@ import com.qa.springpups.repo.OwnerRepo;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@Sql(scripts = {"classpath:pup-schema.sql", "classpath:pup-data.sql"},executionPhase = ExecutionPhase.BEFORE_TEST_METHOD )
 public class OwnerControllerIntegrationTest {
 
 	@Autowired
@@ -43,63 +45,61 @@ public class OwnerControllerIntegrationTest {
 	@Autowired
 	private ModelMapper modelMapper; 
 	
-	private Long id; 
-	private Owner testOwner; 
-	private Owner testOwnerWithId; 
-	private OwnerDTO ownerDTO;
-	
 	private OwnerDTO mapToDTO(Owner owner) {
 		return this.modelMapper.map(owner,OwnerDTO.class);
 	}
 	
 	private final String URI = "/owner";
+	private final Owner OWNER_FROM_DB = new Owner(1L,"Savannah", 22);
+	private final Long id = OWNER_FROM_DB.getId();
 	
-	@BeforeEach
-	void init() {
-		this.repo.deleteAll(); 
-		this.testOwner = new Owner("Savannah", 22);
-		this.testOwnerWithId = this.repo.saveAndFlush(testOwner);
-		this.id = testOwnerWithId.getId();
-		this.ownerDTO = this.mapToDTO(testOwnerWithId);
-	}
+//	@BeforeEach
+//	void init() {
+//		this.repo.deleteAll(); 
+//		this.testOwner = new Owner("Savannah", 22);
+//		this.testOwnerWithId = this.repo.saveAndFlush(testOwner);
+//		this.id = testOwnerWithId.getId();
+//		this.ownerDTO = this.mapToDTO(testOwnerWithId);
+//	}
 	
 	@Test
 	void createTest() throws JsonProcessingException, Exception {
+		final Owner NEW_OWNER = new Owner("Bark Whalberg", 34);
+		final Owner SAVED_OWNER = new Owner(2L, "Bark Whalberg", 34);
 		this.mock.perform(post(URI +"/create")
 					.contentType(MediaType.APPLICATION_JSON)
-					.content(this.mapper.writeValueAsString(this.testOwner)))
+					.content(this.mapper.writeValueAsString(NEW_OWNER)))
 			.andExpect(status().isCreated())
-			.andExpect(content().json(this.mapper.writeValueAsString(this.ownerDTO)));
+			.andExpect(content().json(this.mapper.writeValueAsString(this.mapToDTO(SAVED_OWNER))));
 	}
 	
 	@Test
 	void readAllTest() throws JsonProcessingException, Exception {
-		final List<Owner> OWNERS = new ArrayList<>();
-		OWNERS.add(this.testOwnerWithId);
+		final List<OwnerDTO> OWNERS = new ArrayList<>();
+		OWNERS.add(this.mapToDTO(OWNER_FROM_DB));
 		
 		this.mock.perform(get(URI+"/readAll")
 					.contentType(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
-				.andExpect(content().json(this.mapper.writeValueAsString(OWNERS)));
+				.andReturn().getResponse().getContentAsString();
 	}
 	
 	@Test
 	void readOneTest() throws JsonProcessingException, Exception {
-		this.mock.perform(get(URI + "/readOne/" + this.id)
+		this.mock.perform(get(URI+"/readOne/"+ this.id)
 					.contentType(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
-				.andExpect(content().json(this.mapper.writeValueAsString(this.testOwnerWithId)));
+				.andExpect(content().json(this.mapper.writeValueAsString(this.mapToDTO(OWNER_FROM_DB))));
 	}
 	
 	@Test
 	void updateTest() throws JsonProcessingException, Exception {
 		final Owner UPDATED = new Owner(this.id,"Savannah", 23);
-		this.ownerDTO = this.mapToDTO(UPDATED); 
-		this.mock.perform(put(URI+"/update/" + this.id)
+		this.mock.perform(put(URI+"/update/"+ this.id)
 					.contentType(MediaType.APPLICATION_JSON)
-					.content(this.mapper.writeValueAsString(this.ownerDTO)))
+					.content(this.mapper.writeValueAsString(UPDATED)))
 				.andExpect(status().isAccepted())
-				.andExpect(content().json(this.mapper.writeValueAsString(this.ownerDTO)));
+				.andExpect(content().json(this.mapper.writeValueAsString(this.mapToDTO(UPDATED))));
 	}
 	
 	@Test
