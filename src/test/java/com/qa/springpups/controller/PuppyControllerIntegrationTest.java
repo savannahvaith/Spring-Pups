5bc593aa45d08dba02bf7d07141181122dfe220b
savannahvaith <sav.vaithilingam@gmail.com>
2021-04-15 10:17:13 +0100

    SV: Added puppy integration test

--- src/test/java/com/qa/springpups/controller/PuppyControllerIntegrationTest.java
+++ src/test/java/com/qa/springpups/controller/PuppyControllerIntegrationTest.java
@@ -1,9 +1,17 @@
 package com.qa.springpups.controller;
 
+import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,16 +23,18 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultMatcher;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.qa.springpups.domain.Puppy;
 import com.qa.springpups.dto.PuppyDTO;
+import com.qa.springpups.repo.PuppyRepo;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class PuppyControllerIntegrationTest {
 
 	@Autowired
-	private MockMvc movkMvc;
+	private MockMvc mockMvc;
 	
 	@Autowired
 	private ObjectMapper mapper; 
@@ -32,36 +42,90 @@ public class PuppyControllerIntegrationTest {
 	@Autowired
 	private ModelMapper modelMapper;
 	
+	@Autowired
+	private PuppyRepo repo; 
+	
 	private final String URI = "/puppy";
 	
 	private PuppyDTO mapToDTO(Puppy p ) {
 		return this.modelMapper.map(p,PuppyDTO.class);
 	}
 	
-	private final static Puppy puppy = new Puppy("Milo", 2, "Lahsa Apso", false);
-	private final static Puppy savedPuppy = new Puppy(1L,puppy.getName(), puppy.getAge(), puppy.getBreed(), puppy.isSkill());
+	private Long id; 
+	private Puppy testPuppy; 
+	private Puppy testPuppyWithId; 
+	private PuppyDTO puppyDTO; 
+	
+	@BeforeEach
+	void init() {
+		this.repo.deleteAll();
+		this.testPuppy = new Puppy("Milo", 2, "Lahsa Apso", false);
+		this.testPuppyWithId = this.repo.saveAndFlush(this.testPuppy);
+		this.id = this.testPuppyWithId.getId();
+		this.puppyDTO = mapToDTO(testPuppyWithId);
+	}
 	
 	@Test
 	void createTest() throws Exception {
 		
-		// create puppy 
-		// convert from object to string 
-		String puppyAsString = this.mapper.writeValueAsString(puppy);
-		
-		// make the request
-		RequestBuilder mockRequest = post(URI + "/create")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(puppyAsString);
 
-		// Response stuff 
-		// need to convert to DTO for relationship tings
-		String savedPuppyAsString = this.mapper.writeValueAsString(this.mapToDTO(savedPuppy));
+//		// convert from object to string 
+//		String puppyAsString = this.mapper.writeValueAsString(this.testPuppy);
+//		
+//		// make the request
+//		RequestBuilder mockRequest = post(URI + "/create")
+//				.contentType(MediaType.APPLICATION_JSON)
+//				.content(puppyAsString);
+//
+//		// Response stuff 
+//		// need to convert to DTO for relationship tings
+//		String savedPuppyAsString = this.mapper.writeValueAsString(this.puppyDTO);
+//		
+//		ResultMatcher resultStatus = status().isCreated(); 
+//		ResultMatcher resultBody = content().json(savedPuppyAsString);
+//	
+//		this.mockMvc.perform(mockRequest).andExpect(resultStatus).andExpect(resultBody);
 		
-		ResultMatcher resultStatus = status().isCreated(); 
-		ResultMatcher resultBody = content().json(savedPuppyAsString);
+		this.mockMvc.perform(post(URI+"/create")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(this.mapper.writeValueAsString(this.testPuppy)))
+					.andExpect(status().isCreated())
+					.andExpect(content().json(this.mapper.writeValueAsString(this.puppyDTO)));
+	}
 	
-		this.movkMvc.perform(mockRequest).andExpect(resultStatus).andExpect(resultBody);
+	@Test
+	void readOneTest() throws JsonProcessingException, Exception {
+		this.mockMvc.perform(get(URI+"/getOne/"+this.id)
+					.contentType(MediaType.APPLICATION_JSON))
+					.andExpect(status().isOk())
+					.andExpect(content().json(this.mapper.writeValueAsString(this.puppyDTO)));
+	}
 	
+	@Test
+	void readAllTest() throws JsonProcessingException, Exception {
+		final List<PuppyDTO> PUPPIES = new ArrayList<>();
+		PUPPIES.add(this.puppyDTO);
+		
+		this.mockMvc.perform(get(URI+"/getAll")
+							.contentType(MediaType.APPLICATION_JSON))
+					.andExpect(status().isOk())
+					.andExpect(content().json(this.mapper.writeValueAsString(PUPPIES)));
+	}
+	
+	@Test
+	void updateTest() throws JsonProcessingException, Exception {
+		final PuppyDTO NEW_PUPPY = new PuppyDTO(this.id, "Tyson", 2, "German Shepard", false);
+		this.mockMvc.perform(put(URI + "/update/" + this.id)
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(this.mapper.writeValueAsString(NEW_PUPPY)))
+				.andExpect(status().isAccepted())
+				.andExpect(content().json(this.mapper.writeValueAsString(NEW_PUPPY)));
+	}
+	
+	@Test
+	void deleteTest() throws Exception {
+		this.mockMvc.perform(delete(URI + "/delete/" + this.id))
+				.andExpect(status().isNoContent());
 	}
 	
 }
